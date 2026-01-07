56fbf6881227b5b8e559c5f255405dc81402759f
jonahc44 <jonahc0508@gmail.com>
2026-01-06 22:26:36 -0500

    increase json limit of express app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -121,7 +121,7 @@ app.use(session({
   },
 }))
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 if (process.env.ENV == 'dev') {
   console.log('Operating in dev environment');
